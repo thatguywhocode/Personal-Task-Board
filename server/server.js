93fce02d34ed1f88f0306114758e0db6f891fc29
thatguywhocode <138510462+thatguywhocode@users.noreply.github.com>
2026-06-01 21:27:37 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,16 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://personal-task-board.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Test Route
